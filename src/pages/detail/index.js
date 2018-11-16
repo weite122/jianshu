7f76d457db57411c162b3cecac5e2b2ccf5c189f
weite122 <949193531@qq.com>
2018-11-16 14:24:45 +0800

    [A]Detail基础样式完成

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import { DetailWrapper, Header, Content } from "./style";
 
 class Detail extends Component {
   render() {
     return (
-      <div>Detail</div>
+      <DetailWrapper>
+        <Header>15条让内心变强大的哲理</Header>
+        <Content>
+          <img src="//upload-images.jianshu.io/upload_images/12961714-3318dd46510b0806.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240" alt=""/>
+          <p>15条让内心变强大的哲理15条让内心变强大的哲理</p>
+          <p>15条让内心变强大的哲理15条让内心变强大的哲理</p>
+          <p>15条让内心变强大的哲理15条让内心变强大的哲理</p>
+          <p>15条让内心变强大的哲理15条让内心变强大的哲理</p>
+        </Content>
+      </DetailWrapper>
     )
   }
 }
