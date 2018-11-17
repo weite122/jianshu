22fa6e736e631a38f69790c4c3616ab9c6c68622
weite122 <949193531@qq.com>
2018-11-17 19:56:54 +0800

    [A]获取动态路由参数

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -16,7 +16,7 @@ class Detail extends Component {
   }
 
   componentDidMount() {
-    this.props.getDetail()
+    this.props.getDetail(this.props.match.params.id)
   }
 }
 
@@ -26,8 +26,8 @@ const mapState = (state) => ({
 })
 
 const mapDispatch = (dispatch) => ({
-  getDetail() {
-    dispatch(actionCreators.getDetail())
+  getDetail(id) {
+    dispatch(actionCreators.getDetail(id))
   }
 })
 export default connect(mapState, mapDispatch)(Detail)
