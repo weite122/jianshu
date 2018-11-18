9ee8dd8978e3f6680371824e439a619fee8248c9
weite122 <949193531@qq.com>
2018-11-18 23:59:47 +0800

    [A]login

---
+++ src/pages/login/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { DetailWrapper, Header, Content } from "./style";
+
+
+class Detail extends Component {
+  render() {
+    return (
+      <div>login</div>
+    )
+  }
+
+
+}
+
+const mapState = (state) => ({
+
+})
+
+const mapDispatch = (dispatch) => ({
+
+})
+export default connect(mapState, mapDispatch)(Detail)
