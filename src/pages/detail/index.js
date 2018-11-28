f09a1a22abecc0592990077f025d5da20e08acd6
weite122 <949193531@qq.com>
2018-11-29 00:00:00 +0800

    [A]使用withRouter

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { DetailWrapper, Header, Content } from "./style";
+import { withRouter } from 'react-router-dom'
+import { DetailWrapper, Header, Content } from "./style"
 import { actionCreators } from './store'
 
 class Detail extends Component {
@@ -30,4 +31,4 @@ const mapDispatch = (dispatch) => ({
     dispatch(actionCreators.getDetail(id))
   }
 })
-export default connect(mapState, mapDispatch)(Detail)
+export default connect(mapState, mapDispatch)(withRouter(Detail))
